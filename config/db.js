eff421ad353b94ffd4e152a7489552c546420faf
akshit <akshit10.anand2004@gmail.com>
2025-01-04 15:06:44 +0530

    db config done

---
+++ config/db.js
@@ -0,0 +1,29 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL for secure connection
+      rejectUnauthorized: false, // Skip SSL certificate verification (Supabase-specific)
+    },
+  },
+  logging: true, //ill set it to false later
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.message);
+  }
+})();
+
+export default sequelize; 
